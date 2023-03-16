9aa32e66207abad7edda10ed1e4530fd8bbbf432
BrisaAguayo <124205663+BrisaAguayo@users.noreply.github.com>
2023-03-16 19:57:41 +0000

    mensajeeee

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 
 namespace Pelicula
 {
+    
 
     public class Pelicula
     {
-        //Campos
+        //Campos--
+        string titulo;
+        string descripcion;
+        int duracion;
+        int año;
+        string director;
         private List<Actor> actores = new List<Actor>();
 
-        //Constructores
+
+        //Constructores--
+        public static Pelicula(string titulo, int año, string descripcion, string actores, string director, int duracion)
+
+
+        //Métodos--
      
-        //Métodos
-     
-        public void Imprime()
+        public void imprime_Pelicula()
         {
-     //       Console.WriteLine($"{titulo} ({año})");
+          Console.WriteLine($"{titulo} {año} {descripcion} {actores} {director} {duracion}");
 
         }
+
+        string titulo {set; get;};
+        string descripcion {set; get;);
+
 
 
     }
@@ -25,6 +38,8 @@ namespace Pelicula
     public class Actor
     {
         //Propiedades
+        string nombre;
+
 
         //Constructores
 
@@ -39,13 +54,16 @@ namespace Pelicula
     // Puedes probar tu código en Main() pero lo importante
     // es que pase las pruebas
 
-    class Program
-    {
+class Program 
+{
+static void Main(){
 
+    Pelicula p1 = new Pelicula();
+    p1.SetTitulo("La La Land");
+    p1.SetAño(2016);
+    Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello");
-        }
+}
+} 
     }
 }
